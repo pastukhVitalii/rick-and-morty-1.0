e1ca52ca22857e7628bc427b8849b9522906123c
pastukhVitalii <pastukh.v.7@gmail.com>
2020-11-30 19:29:53 +0200

    added checkboxes to myWatchList page

--- src/pages/MyWatchList/Episode.tsx
+++ src/pages/MyWatchList/Episode.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import {Delete} from "@material-ui/icons";
-import {IconButton} from "@material-ui/core";
+import {Checkbox, IconButton} from "@material-ui/core";
 import {EpisodeListType} from "./MyWatchList";
 
 type PropsType = {
     episode: EpisodeListType
     removeEpisode: (id: number) => void
+    changeStatusEpisode: (id: number) => void
 }
 
 export const Episode = React.memo((props: PropsType) => {
 
     return (
         <>
-            <span>{props.episode.title}</span>
-            <IconButton onClick={() => props.removeEpisode(props.episode.id)}>
-                <Delete/>
-            </IconButton>
+            <div>
+                <Checkbox
+                    checked={props.episode.status}
+                    color="primary"
+                    onChange={() => props.changeStatusEpisode(props.episode.id)}
+                />
+                <span>{props.episode.title}</span>
+                <span>
+                    <IconButton onClick={() => props.removeEpisode(props.episode.id)}>
+                    <Delete/>
+                </IconButton>
+                </span>
+            </div>
         </>
     )
 })
--- src/pages/MyWatchList/MyWatchList.tsx
+++ src/pages/MyWatchList/MyWatchList.tsx
@@ -5,6 +5,7 @@ import {Episode} from "./Episode";
 export type EpisodeListType = {
     id: number
     title: string
+    status: boolean
 }
 
 export const MyWatchList = React.memo(() => {
@@ -35,15 +36,27 @@ export const MyWatchList = React.memo(() => {
                 nextId = e.id + 1
             }
         })
-        saveLocalStorage([...episodes, {id: nextId, title: title}]);
-        handlerEpisodes([...episodes, {id: nextId, title: title}]);
+        saveLocalStorage([...episodes, {id: nextId, title: title, status: false}]);
+        handlerEpisodes([...episodes, {id: nextId, title: title, status: false}]);
     }
 
     const removeEpisode = (id: number) => {
 
-        let idt = episodes.findIndex(e => e.id === id);
-        let newList = [...episodes.slice(0, idt), ...episodes.slice(idt + 1)];
+        let idE = episodes.findIndex(e => e.id === id);
+        let newList = [...episodes.slice(0, idE), ...episodes.slice(idE + 1)];
 
+        saveLocalStorage(newList);
+        handlerEpisodes(newList);
+    }
+    const changeStatusEpisode = (id: number) => {
+
+        let newList = episodes.map(e => {
+            if (e.id !== id) {
+                return e
+            } else {
+                return {...e, status: !e.status}
+            }
+        })
         saveLocalStorage(newList);
         handlerEpisodes(newList);
     }
@@ -51,7 +64,7 @@ export const MyWatchList = React.memo(() => {
     return (
         <div style={{paddingTop: '20px'}}>
             <AddItemForm addItem={addTask} disabled={false}/>
-            {episodes.map(e => <Episode key={e.id} episode={e} removeEpisode={removeEpisode}/>)}
+            {episodes.map(e => <Episode key={e.id} episode={e} removeEpisode={removeEpisode} changeStatusEpisode={changeStatusEpisode}/>)}
         </div>
     );
 })
